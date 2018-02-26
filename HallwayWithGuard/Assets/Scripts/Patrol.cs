99e72e032d462907819eedbb7461fee73b33f44f
Stanley <Stanley@github.net>
2018-02-26 15:07:26 -0500

    Update_SD

---
+++ HallwayWithGuard/Assets/Scripts/Patrol.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour {
+
+	public float speed;
+	private float waitTime;
+	public float startWaitTime;
+
+	public Transform[] Location;
+	private int randomLocation;
+
+
+	void Start () {
+		waitTime = startWaitTime;
+		randomLocation = Random.Range(0,Location.Length);
+	}
+	
+	
+	void Update () {
+		transform.position = Vector3.MoveTowards (transform.position, Location [randomLocation].position, speed * Time.deltaTime);
+        
+		if (Vector3.Distance (transform.position, Location [randomLocation].position) < 0.2f) {
+			if (waitTime <= 0) {
+				randomLocation = Random.Range (0, Location.Length);
+				waitTime = startWaitTime;
+			} else {
+				waitTime -= Time.deltaTime;
+			} 
+		}
+	}
+}
---
+++ HallwayWithGuard/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+
+	public enum RotationAxis
+	{
+		MouseX = 1,
+		MouseY = 2
+	}
+
+	public RotationAxis axes = RotationAxis.MouseX;
+
+	public float minV = -45.0f;
+	public float maxV = 45.0f;
+
+	public float Horizontal = 10.0f;
+	public float Vertical = 10.0f;
+
+	public float rotationX = 0;
+
+
+
+	void Update () {
+		if (axes == RotationAxis.MouseX) {
+			transform.Rotate (0, Input.GetAxis ("Mouse X") * Horizontal, 0);
+		} else if (axes == RotationAxis.MouseY) {
+			rotationX -= Input.GetAxis ("Mouse Y") * Vertical;
+			rotationX = Mathf.Clamp (rotationX, minV, maxV);
+
+			float rotationY = transform.localEulerAngles.y;
+
+			transform.localEulerAngles = new Vector3 (rotationX, rotationY, 0);
+		}
+
+	}
+}
